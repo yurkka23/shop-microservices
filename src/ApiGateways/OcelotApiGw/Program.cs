e95c28c1d0632ac506f5898d6d829065a7431a59
yurii <ykarabin@exoft.net>
2023-09-03 19:44:28 +0300

    Implement Ocelot Api Gateway

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host
+.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+})
+.ConfigureLogging((hostingContext, loggingbuilder) =>
+{
+    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingbuilder.AddConsole();
+    loggingbuilder.AddDebug();
+});
+
+builder.Services.AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+
+app.Run();
+
